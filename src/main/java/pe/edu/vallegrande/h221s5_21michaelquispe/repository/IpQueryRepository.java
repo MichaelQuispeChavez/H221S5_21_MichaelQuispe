82a371ac3b141b4f7a9f6c45dc69b75cdd0a6d96
MichaelJQC <105119643+MichaelQuispeChavez@users.noreply.github.com>
2024-07-15 20:19:30 +0000

    v1

--- src/main/java/pe/edu/vallegrande/h221s5_21michaelquispe/repository/IpQueryRepository.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.vallegrande.h221s5_21michaelquispe.repository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import pe.edu.vallegrande.h221s5_21michaelquispe.model.IpQuery;
-
-public interface IpQueryRepository extends ReactiveMongoRepository<IpQuery , String> {
-}
